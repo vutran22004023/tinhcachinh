f372c77346599cad43db3053451e8801607e3fe3
admin <admin@DESKTOP-14JLFHH>
2023-05-05 22:26:26 +0700

    Add project files.

---
+++ ConsoleApp1/HINH.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    abstract class HINH
+    {
+        protected string tenHinh;
+
+        public string TenHinh { get => this.tenHinh; set => this.tenHinh = value; }
+
+        public HINH(){}
+
+        public HINH (string tenHinh)
+        {
+            this.TenHinh = tenHinh;
+        }
+
+        public virtual void Nhap ()
+        {
+            Console.WriteLine("Nhap Hinh :");
+            this.tenHinh = Console.ReadLine();
+        }
+        public virtual void Xuat()
+        {
+            Console.WriteLine("Hinh: " + tenHinh);
+        }
+        
+    }
+
+
+}
---
+++ ConsoleApp1/TAMGIAC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+     class TAMGIAC :HINH
+    {
+        private float A;
+        private float B;
+        private float C;
+
+        public float A1 { get => this.A; set => this.A = value; }
+        public float B1 { get => this.B; set => this.B = value; }
+        public float C1 { get => this.C; set => this.C = value; }
+
+        public TAMGIAC(): base() { }
+
+        public TAMGIAC(float a, float b, float c): base()
+        {
+            A = a;
+            B = b;
+            C = c;
+            
+        }
+    }
+}
